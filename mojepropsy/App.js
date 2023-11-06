1eaa35cca9228618c6e048e7b6e88025e610d6f1
JS <JS@wp.pl>
2023-11-06 09:43:10 +0100

    Add a new component KingsList

--- mojepropsy/App.js
+++ mojepropsy/App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import KingsList from './KingsList';
+
+
 export default function App() {
+  let kings = [
+    {id:1, name:"Bolesław Chrobry", reignDate:"992-1025"},
+    {id:2, name:"Mieszko II Lambert", reignDate:"1025-1031"},
+    {id:3, name:"Bolesław Śmiały", reignDate:"1058-1079"},
+    {id:4, name:"Przemysław II", reignDate:"1295-1206"},
+  ]
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <KingsList kingsList={kings} />
     </View>
   );
 }
---
+++ mojepropsy/PolishKing.js
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native' ;
+
+const PolishKing = ({name})=>{
+
+    return(
+        <View>
+            <Text>Polish King {name} is comming</Text>
+        </View> 
+    )
+}
+
+export default PolishKing;
